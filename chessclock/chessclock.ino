59d1658a9ba17a2a7813a179591ddbcf87b9a187
BartDeWaal <bart@abnormail.org>
2013-08-30 22:59:38 +0200

    use fdsString for passing the string

--- chessclock/chessclock.ino
+++ chessclock/chessclock.ino
@@ -4,7 +4,7 @@ Aansturen FDS132 LED matrix bord m.b.v. SPI.
 Toont karakters op line 0.  
 */  
 #include <SPI.h>                                       // Om snelheid te winnen maken we gebruik van SPI.  
-#include <fds132text.h>                                                      // Daarom halen we de SPI library erbij.  
+#include "fds132text.h"                                                      // Daarom halen we de SPI library erbij.  
                                                        // Pinnen voor het aansturen van het FDS132 bord.  
 const int strobePin = 10;                              // pin voor strobe signaal, ook wel latch genaamd.  
 const int clockPin = 13;                               // pin met clock signaal.  
@@ -17,6 +17,8 @@ int row;                                               // int voor bijhouden wel
 
 
 fdsChar letterA;
+fdsChar fdsColon;
+fdsString maintext;
 
 void initialiseLetters(){
     letterA.character_map[0]=B00011000;
@@ -25,8 +27,37 @@ void initialiseLetters(){
     letterA.character_map[3]=B00111100; 
     letterA.character_map[4]=B00100100; 
     letterA.character_map[5]=B01000010; 
-    letterA.character_map[6]=B01000010;     
+    letterA.character_map[6]=B01000010;
+    letterA.width=8;
+    
+    fdsColon.character_map[0]=B00000000;
+    fdsColon.character_map[1]=B00000000;
+    fdsColon.character_map[2]=B10000000;
+    fdsColon.character_map[3]=B00000000;
+    fdsColon.character_map[4]=B10000000;
+    fdsColon.character_map[5]=B00000000;
+    fdsColon.character_map[6]=B00000000;
+    fdsColon.width=2;
+    
+    maintext.value = &letterA;
+    maintext.rest = 0;
+    
+    addToString(&maintext, &fdsColon);
+    addToString(&maintext, &fdsColon);
+    addToString(&maintext, &letterA);
+    addToString(&maintext, &fdsColon);
+}
 
+void addToString(fdsString *string, fdsChar *addition){
+    //find end of string
+    while(string -> rest != 0) {
+        string = string -> rest; 
+    }
+    //add the new character
+    string -> rest = (fdsString *) malloc(sizeof(struct fdsString));
+    string = string -> rest;
+    string -> value = addition;
+    string -> rest = 0;
 }
 
 void setup() {   
@@ -51,7 +82,7 @@ void loop()
         digitalWrite (resredPin, LOW);                     // en we doven de display om ghosting te voorkomen.  
         setRow(row);                                       // we sturen alle zeven rijen aan.  
         zeroDisplay();                                     // alle uitgangen van de shiftregisters op nul.  
-        setDisplay(row);                                   // haal de te tonen data op.   
+        setDisplay(row, maintext);                                   // haal de te tonen data op.   
         digitalWrite(strobePin, HIGH);                     // update de shiftregisters.  
         digitalWrite (resredPin, HIGH);                    // en zet display terug aan.  
         delayMicroseconds(2500);                           // pauseren want de update gaat te vlug.   
@@ -71,9 +102,30 @@ void zeroDisplay()                                     // deze functie zet alle 
        SPI.transfer(0);                                     // door 34 keer een byte met waarde nul te sturen.  
    }  
 }  
-void setDisplay(int row)                               // haal de tekstdata uit de eerder gedefinieerde map met karakters.  
-{  
-    SPI.transfer(letterA.character_map[row]);   
-  
+void setDisplay(int row, fdsString outputText)                               // haal de tekstdata uit de eerder gedefinieerde map met karakters.  
+{   
+    int i = 0;
+    int currentbit = 0;
+    byte outputbytes[34];
+    for(i=0; i<34; i++)  
+    {  
+       outputbytes[i] = 0;
+    }
+    
+    // unpack the fdsString
+    fdsString *current = &outputText;
+    while (i<2000) {
+        i++;
+        if (current == 0) {break;}
+        outputbytes[currentbit/8] = (*(*current).value).character_map[row];
+        currentbit += 8;
+        current = current -> rest;
+    }
+    
+    for(i=34; i>=0; i--){
+        SPI.transfer(outputbytes[i]);   
+    };
+
 }  
+
 
