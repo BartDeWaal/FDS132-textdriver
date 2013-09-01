b7566d61e5ebbcdd7fe9f25f813a9687706e4f5e
BartDeWaal <bart@abnormail.org>
2013-09-01 16:52:31 +0200

    Remove global variable row

--- chessclock/chessclock.ino
+++ chessclock/chessclock.ino
@@ -5,11 +5,9 @@ Aansturen FDS132 LED matrix bord m.b.v. SPI.
 #include <SPI.h>            // Om snelheid te winnen maken we gebruik van SPI.  
 #include "fds132text.h"     
 
-// Pinnen voor het aansturen van het FDS132 bord.  
-int row;                    // int voor bijhouden welke rij we aansturen.  
 
 
-fdsScreen mainScreen = fdsScreen("Help me, I'm stuck!", 0);
+fdsScreen mainScreen = fdsScreen("03:01:02 00:00:00   0123456789", 0);
 
 void setup() {   
    pinMode (strobePin, OUTPUT);                          // zet alle pinnen als output om de shift registers aan te sturen.  
@@ -28,12 +26,14 @@ void setup() {
 }  
 void loop()  
 {  
-    for (row=0; row<7; row++)                            // rij teller.  
+    for (int row=0; row<7; row++)                            // rij teller.  
     {   
         digitalWrite(strobePin, LOW);                      // strobePin laag opdat de LEDs niet wijzigen als we de bits doorsturen.  
         digitalWrite (resredPin, LOW);                     // en we doven de display om ghosting te voorkomen.  
         setRow(row);                                       // we sturen alle zeven rijen aan.  
-        setDisplay(row);
+        for(int i=34; i>=0; i--){
+            SPI.transfer(mainScreen.output[row][i]);   
+        };
         digitalWrite(strobePin, HIGH);                     // update de shiftregisters.  
         digitalWrite (resredPin, HIGH);                    // en zet display terug aan.  
         delayMicroseconds(2500);                           // pauseren want de update gaat te vlug.   
@@ -54,12 +54,3 @@ void zeroDisplay()                                     // deze functie zet alle 
    }  
 }  
 
-
-// Unpack the required row from the StringNode
-void setDisplay(int row)
-{   
-    // Put the bytes on the display, last one first  (because the first one entered will be shifted to the end)
-    for(int i=34; i>=0; i--){
-        SPI.transfer(mainScreen.output[row][i]);   
-    };
-}
